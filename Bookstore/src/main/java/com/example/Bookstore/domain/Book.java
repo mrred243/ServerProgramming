e6d5351d0f1020fb28ceff924089ce254c6d5af9
ThienAn <mrred243@gmail.com>
2019-09-14 23:35:14 +0300

    First commit

---
+++ Bookstore/src/main/java/com/example/Bookstore/domain/Book.java
@@ -0,0 +1,11 @@
+package com.example.Bookstore.domain;
+
+public class Book {
+	private long id;
+	private String msg;
+	private String title;
+	private String author; 
+	private long year;
+	private long isbn;
+	private long price;
+}
---
+++ Bookstore/src/main/java/com/example/Bookstore/web/BookstoreController.java
@@ -0,0 +1,16 @@
+package com.example.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookstoreController {
+	
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String greetingFrom(Model model) {
+		return "index";
+	}
+
+}
