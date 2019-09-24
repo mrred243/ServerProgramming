491d117c7958fbe516f142071fb006a207830013
ThienAn <mrred243@gmail.com>
2019-09-24 16:22:35 +0300

    Design bookstore database

--- Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,7 +1,14 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -10,4 +17,21 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo (BookRepository repository, CategoryRepository crepository) {
+		return (args) -> {
+			Category c1 = new Category ("Crime");
+			Category c2 = new Category ("Novel");
+
+			
+			crepository.save(c1);
+			crepository.save(c2);
+
+			Book b1 = new Book ("Juha", "React", 50, 22222, 2006, crepository.findByName("Crime").get(0) );
+			
+			repository.save(b1);
+		};
+	}
+		
+	
 }
---
+++ Bookstore/src/main/java/com/example/Bookstore/Controller/BookController.java
@@ -0,0 +1,54 @@
+package com.example.Bookstore.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.CategoryRepository;
+
+@Controller
+public class BookController {
+ @Autowired
+ private BookRepository repository;
+ 
+ @Autowired
+ private CategoryRepository crepository;
+ 
+ @RequestMapping("/booklist")
+ public String bookList(Model model) {
+	 model.addAttribute("books", repository.findAll());
+	 return "booklist";
+ }
+ 
+ @RequestMapping(value = "/add")
+ public String addBook(Model model) {
+	 model.addAttribute("book", new Book());
+	 model.addAttribute("categories", crepository.findAll());
+	 return "addbook";
+ }
+ 
+ @RequestMapping(value = "/save", method = RequestMethod.POST)
+ public String save(Book book){
+     repository.save(book);
+     return "redirect:booklist";
+ }  
+ 
+ @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+ public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+ 	repository.deleteById(bookId);
+     return "redirect:../booklist";
+ }
+ 
+ @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+ public String editBook(@PathVariable("id") Long bookId, Model model) {
+ 	model.addAttribute("book", repository.findById(bookId));
+ 	model.addAttribute("categories", crepository.findAll());
+ 	return "editbook";
+ } 
+
+}
